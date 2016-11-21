00cd240726538fd233deac88f0e1f997342c659f
lindsayhale <lh476@cornell.edu>
2016-11-21 18:41:31 -0500

    Add files via upload

---
+++ robot.c
@@ -0,0 +1,37 @@
+ /*
+ * main.c
+ *
+ *
+ *  Created on: Nov 17, 2015
+ *      Author: Lindsay, Jonah, Andrew
+ */
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "serial.h"
+#include <util/delay.h>
+
+#include "motor.h"
+#include "active.h"
+#include "QTI.h"
+#include "sonar.h"
+
+ISR(){
+	//Interrupt
+}
+
+
+int main(void){
+	//choose pin
+	//set as input/output
+	//interrupt stuff
+	//enable shit
+	sei();
+	while(){
+		//loop that the robot goes through
+	}
+	//shut down robot when white line
+
+}
