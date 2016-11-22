a7857040869c6357355b2b8e5df9ff2f41d3c896
lindsayhale <lindsayhale@users.noreply.github.com>
2016-11-22 01:40:12 -0500

    Update QTI1.h

--- QTI1.h
+++ QTI1.h
@@ -5,7 +5,6 @@
  *  Author: MAE_Labs
  */ 
 
-
 #ifndef QTI1_H_
 #define QTI1_H_
 
@@ -13,21 +12,24 @@ void killQTI(int pin){
 	//when center QTI goes off kill the robot
 	//stop driving
 	//turn all 4 wheels off
-	
+	if (~PIND & 1<<pin){
+		PORTD &= (0<<PD6) | (0<<PD5); //stops
+	}
+	//turn on LED
+	//set pin w LED to output
+	//set value of output to 1
 }
 
 void alertQTI(int pin1, int pin2){
 	//front QTI senses white
 	//send left, right, both to main??
+	//do I need this??
 }
 
-void setupQTI(int pin1, int pin2, int pin3){ //make applicable for all QTI
+void setupQTI(int pin1, int pin2, int pin3){ 
 	//DDRD |= (1<<pin1) | (1<<pin2) | (1<<pin3);
 	PCMSK2 |= (1<<pin1) | (1<<pin2) | (1<<pin3);
 	PCICR |= (1 << PCIE2); 
 }
-
-
-
 
 #endif /* QTI1_H_ */
