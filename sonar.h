89216935332cc0da5e3d450668678524401d9375
lindsayhale <lh476@cornell.edu>
2016-11-21 19:20:49 -0500

    Update sonar.h

--- sonar.h
+++ sonar.h
@@ -11,16 +11,13 @@
  */ 
 //volatile int itime;
 
-void initSonar()
-{
-	//printf("initSonar \n");
+void initSonar(){
 	TCCR1B |= 0b00000011; //sets CS11 and CS12 to 1
 	TCCR1B &= 0b11111011; //sets CS13 to zero
 	PCICR |= 0b00000001; //sets PC I/O to 1
 }
 
 void startSonarMeasurement(){
-	//printf("start \n");
 	DDRB |= 0b00000001; //pin 8, PB0, PCINT0
 	PORTB |= 0b00000001;
 	_delay_us(5);
@@ -29,8 +26,8 @@ void startSonarMeasurement(){
 	PCMSK0 |= 0b00000001; //sets PCINT0 to 1
 }
 
-/*ISR(PCINT0_vect)
-{
+/*
+ISR(PCINT0_vect){
 	unsigned char sreg;
 	if(PINB & 0b00000001){
 		sreg = SREG;
@@ -41,21 +38,20 @@ void startSonarMeasurement(){
 		PCMSK0 &= 0b11111110; //sets PCINT0 to 0
 		itime = TCNT1;
 	}
-}*/
+}
+*/
 
 void getSonar(){
-	//printf("getSonar\n");
 	startSonarMeasurement();
 	int irange;
 	irange = 0.02712 * (float)itime;
 	printf("Distance: %u [inches]\r\n", (uint16_t)irange);
 }
 
-
 void startSonar(){
 	initSonar();
-	while(1)
-	{
+	while(1){
 		getSonar();
 		_delay_ms(500);
 	}
+}
