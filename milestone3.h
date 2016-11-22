44ff71aecb593e33732c7e8f043859536b0b720a
lindsayhale <lindsayhale@users.noreply.github.com>
2016-11-22 01:28:44 -0500

    Update milestone3.h

--- milestone3.h
+++ milestone3.h
@@ -1,20 +1,52 @@
 /*
- * milestone3.h
+ * milestone3.c
  *
  * Created: 11/21/2016 9:32:12 PM
- *  Author: MAE_Labs
+ *  Author: MAE_Labs Lindsay, Jonah, Andy
  */ 
 
+#define F_CPU 16000000UL
 
-#ifndef milestone3_H_
-#define milestone3_H_
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "serial.h"
+#include <util/delay.h>
+#include "QTI1.h"
+#include "wheelie.h"
 
-void turn(){
-	
+volatile int itime;
+
+ISR(PCINT2_vect){
+	//Interrupt for QTI's
+	//FIX ME: does it sense the other robot when front 1 QTI's go off???
+	if (~PIND & 1<<PD2) { //pin PD2 attached to center QTI
+		//go = 0; //stop while loop
+		//killQTI(PD2);
+		//turn LED on
+	} else if ((~PIND & (1<<PD1)) && (~PIND & 1<<PD0)) { //both pin PD1 and PD0
+		straightline();
+		//squareTest();
+	} else if (~PIND & 1<<PD0){ // just pin PD0 (right)
+		straightline();
+		//squareTest();
+	} else if (~PIND & 1<<PD1){ // just pin PD1 (left)
+		straightline();
+		//squareTest();
+	} else { //no white under any sensors
+		drive(0,0);
+	}
 }
 
-void straight(){
-	
+int main(void){
+	//choose pin
+	//set as input/output
+	//interrupt stuff
+	//enable shit
+	init_uart();
+	sei();
+	//startSonar();
+	setupQTI(PD0, PD1, PD2);
+	wheelSetup();
+	drive(0,0);
+	while(1){}
 }
-
-#endif /* milestone3_H_ */
